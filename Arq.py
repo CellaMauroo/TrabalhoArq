671dda733a4b16b531a605d5a9bfcde55a8a534c
Mauro Cella <maurocellaa3301@gmail.com>
2025-02-06 21:54:00 -0300

    Add files via upload

---
+++ Arq.py
@@ -0,0 +1,94 @@
+from abc import ABC, abstractmethod
+
+# Produto
+class Car:
+    def __init__(self):
+        self.engine = None
+        self.seats = None
+        self.trip_computer = None
+        self.gps = None
+
+    def __str__(self):
+        return (f"Car(engine={self.engine}, seats={self.seats}, "
+                f"trip_computer={self.trip_computer}, gps={self.gps})")
+
+# Builder (Interface)
+class CarBuilder(ABC):
+    @abstractmethod
+    def reset(self):
+        pass
+
+    @abstractmethod
+    def set_engine(self, engine):
+        pass
+
+    @abstractmethod
+    def set_seats(self, seats):
+        pass
+
+    @abstractmethod
+    def set_trip_computer(self, trip_computer):
+        pass
+
+    @abstractmethod
+    def set_gps(self, gps):
+        pass
+
+    @abstractmethod
+    def get_result(self):
+        pass
+
+# ConcreteBuilder
+class ConcreteCarBuilder(CarBuilder):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self._car = Car()
+
+    def set_engine(self, engine="V8"):
+        self._car.engine = engine
+        return self  # Permite chamadas encadeadas
+
+    def set_seats(self, seats=5):
+        self._car.seats = seats
+        return self
+
+    def set_trip_computer(self, trip_computer=True):
+        self._car.trip_computer = trip_computer
+        return self
+
+    def set_gps(self, gps=True):
+        self._car.gps = gps
+        return self
+
+    def get_result(self):
+        car = self._car
+        self.reset()  # Prepara o builder para uma nova construção
+        return car
+
+# Director
+class Director:
+    def __init__(self, builder: CarBuilder):
+        self._builder = builder
+
+    def construct_sports_car(self):
+        self._builder.reset()
+        self._builder.set_engine("V12").set_seats(2).set_trip_computer(True).set_gps(True)
+        return self._builder.get_result()
+
+    def construct_family_car(self):
+        self._builder.reset()
+        self._builder.set_engine("V6").set_seats(7).set_trip_computer(False).set_gps(True)
+        return self._builder.get_result()
+
+# Exemplo de uso
+if __name__ == '__main__':
+    builder = ConcreteCarBuilder()
+    director = Director(builder)
+
+    sports_car = director.construct_sports_car()
+    family_car = director.construct_family_car()
+
+    print("Carro Esportivo:", sports_car)
+    print("Carro Familiar:", family_car)
